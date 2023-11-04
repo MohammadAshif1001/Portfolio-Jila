cc9f26911480541bd7a4f1fec7529edb9993b71e
MohammadAshif1001 <mohammadashif1000@gmail.com>
2023-11-04 14:04:58 +0530

    New Protfolio Generator Project

---
+++ frontend/src/components/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
+    <link rel="stylesheet" href="include/style.css">
+    <title>Mohammad Ashif's Creative Portfolio | Portfolio Jila</title>
+</head>
+<body class="bg-blue-500">
+<div id="particles" class="absolute w-full h-full top-0 left-0" style="position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;"></div>
+    <nav>
+        <div class="nav-logo">
+            <a href="#">
+                <img src="include/img/logoNJ.png">
+            </a>
+        </div>
+
+        <ul class="nav-links">
+            <li class="link"><a href="#">Home</a></li>
+            <li id="link1" class="link"><a href="#">Features</a></li>
+            <li id="link2" class="link"><a href="#">Pricing</a></li>
+            <li id="link3" class="link"><a href="#">About</a></li>
+        </ul>
+        <button class="btn">Hire Me</button>
+    </nav>
+
+    <header class="container">
+        <div class="content">
+            <span class="blur"></span>
+            <span class="blur"></span>
+            <h4>CREATE YOUR SITE LIKE A PRO</h4>
+            <H1>Hi, I'm <span>Reza</span>, Web Developer</H1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus rem eos aliquid quo rerum
+                temporibus ipsum distinctio numquam ut omnis placeat, nam sint atque quos dolorem laborum? Rerum, esse
+                dolorem.
+            </p>
+            <button class="btn">Get Started</button>
+            
+        </div>
+        <div class="image">
+            <img src="images/header.png">
+        </div>
+    </header>
---
+++ frontend/src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "protfolio-jila";
+
+$conn = mysqli_connect($dbhost , $dbuser , $dbpass , $dbname);
+
+if(!isset($conn)){
+    echo die("Database connection failed");
+}
+
+$hostname = "http://localhost/protfolio-jila";
+?>
